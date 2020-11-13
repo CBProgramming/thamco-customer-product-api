fb27179a0b080b7552533f1c6f704dea24feb864
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2020-11-13 14:21:18 +0000

    Product EF setup

---
+++ ProductData/ProductDb.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductData
+{
+    public class ProductDb : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductDb(DbContextOptions<ProductDb> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
