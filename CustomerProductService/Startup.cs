92fee6ec4e21f8eec98f566ac72d3587dd70db27
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2021-01-11 19:06:22 +0000

    Trigger pipeline

--- CustomerProductService/Startup.cs
+++ CustomerProductService/Startup.cs
@@ -43,7 +43,7 @@ namespace CustomerProductService
 
         private Microsoft.AspNetCore.Hosting.IHostingEnvironment Env { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
